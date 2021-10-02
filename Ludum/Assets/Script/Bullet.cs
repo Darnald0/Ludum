64b52dc4ef04e51fe124b5abb9f25e2af35bff58
Nolan Jutard <nolan.jutard@gmail.com>
2021-10-02 16:08:27 +0200

    Add Player + Bullet + Background test

---
+++ Ludum/Assets/Script/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int speed;
+
+    private void Start()
+    {
+        StartCoroutine(TimeToDie());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right*speed* Time.deltaTime;
+    }
+
+    IEnumerator TimeToDie()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
+
+}
